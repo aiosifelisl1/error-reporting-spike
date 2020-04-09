a79328fde40b0b1720a9bb2f948a67d3f6749f19
Andreas Iosifelis <aiosifelis@loyalty.com>
2020-04-09 13:10:44 -0400

    Updates

---
+++ precache-manifest.5d4c05df4bb1c892237b3affd7a7f103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082c7649694476d54936261576d823c4",
+    "url": "/error-reporting-spike/index.html"
+  },
+  {
+    "revision": "5ed91f01d61e6eb8f0a9",
+    "url": "/error-reporting-spike/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "992f88c9dc719d3e9329",
+    "url": "/error-reporting-spike/static/js/2.22f5103b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/error-reporting-spike/static/js/2.22f5103b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed91f01d61e6eb8f0a9",
+    "url": "/error-reporting-spike/static/js/main.b055e4b3.chunk.js"
+  },
+  {
+    "revision": "d262e742946359e598ec",
+    "url": "/error-reporting-spike/static/js/runtime-main.1c70fa82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/error-reporting-spike/static/media/logo.5d5d9eef.svg"
+  }
+]);
